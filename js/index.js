ebb16c66ab1c3a085ffa0f9f73a69a5219db8230
kung-kung <mayfire12323@gmail.com>
2023-06-09 14:42:36 +0900

    coffin clear

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(function(){
+    $(".pop").slideDown()
     $(".menu>ul>li, .hd_b").hover(function(){
         // $(".menu_bg").stop().slideDown()
         $(".sub").stop().fadeIn("slow")
@@ -16,5 +17,25 @@ $(function(){
         autoplaySpeed: 2000,
         dots: true,
         arrows: true
+    })
+    $(".sc5_l ul").slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+    })
+    $(".line div").click(function(e){
+        e.preventDefault()
+        $(this).addClass("on")
+        $(this).siblings().removeClass("on")
+    })
+    $(".p_close").click(function(){
+        $(".pop").stop().slideUp()
+    })
+})
+
+$(window).scroll(function(){
+    $(".top").click(function(){
+        $("html, body").animate({scrollTop:0},500)
     })
 })
